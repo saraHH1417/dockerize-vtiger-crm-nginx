50f4989014cf77cdca258121f9748a7d4bac9b50
saraHH1416 <z.haghighi19@gmail.com>
2022-11-08 10:44:18 +0330

    feat: vtiger installed html changes

---
+++ app/html/test/templates_c/v7/734dd33a30c73e5723022291528a76f3162b4140.file.Tag.tpl.php
@@ -0,0 +1,45 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2022-11-07 14:42:38
+         compiled from "/var/www/html/includes/runtime/../../layouts/v7/modules/Vtiger/Tag.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:10761183096369195e4e47b0-13594594%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '734dd33a30c73e5723022291528a76f3162b4140' => 
+    array (
+      0 => '/var/www/html/includes/runtime/../../layouts/v7/modules/Vtiger/Tag.tpl',
+      1 => 1667830458,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '10761183096369195e4e47b0-13594594',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'ACTIVE' => 0,
+    'TAG_MODEL' => 0,
+    'NO_EDIT' => 0,
+    'NO_DELETE' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_6369195e4ed54',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_6369195e4ed54')) {function content_6369195e4ed54($_smarty_tpl) {?>
+ 
+ <span class="tag <?php if ($_smarty_tpl->tpl_vars['ACTIVE']->value==true){?> active <?php }?>" title="<?php echo $_smarty_tpl->tpl_vars['TAG_MODEL']->value->getName();?>
+" data-type="<?php echo $_smarty_tpl->tpl_vars['TAG_MODEL']->value->getType();?>
+" data-id="<?php echo $_smarty_tpl->tpl_vars['TAG_MODEL']->value->getId();?>
+">
+    <i class="activeToggleIcon fa <?php if ($_smarty_tpl->tpl_vars['ACTIVE']->value==true){?> fa-circle-o <?php }else{ ?> fa-circle <?php }?>"></i>
+    <span class="tagLabel display-inline-block textOverflowEllipsis" title="<?php echo $_smarty_tpl->tpl_vars['TAG_MODEL']->value->getName();?>
+"><?php echo $_smarty_tpl->tpl_vars['TAG_MODEL']->value->getName();?>
+</span>
+    <?php if (!$_smarty_tpl->tpl_vars['NO_EDIT']->value){?>
+        <i class="editTag fa fa-pencil"></i>
+    <?php }?>
+    <?php if (!$_smarty_tpl->tpl_vars['NO_DELETE']->value){?>
+        <i class="deleteTag fa fa-times"></i>
+    <?php }?>
+</span><?php }} ?>
---
+++ app/html/test/templates_c/v7/ac071dd4045b06cd468f5285e62db80c775b35f5.file.ModalHeader.tpl.php
@@ -0,0 +1,28 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2022-11-07 14:42:38
+         compiled from "/var/www/html/includes/runtime/../../layouts/v7/modules/Vtiger/ModalHeader.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:16953488096369195e4e1515-21273484%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'ac071dd4045b06cd468f5285e62db80c775b35f5' => 
+    array (
+      0 => '/var/www/html/includes/runtime/../../layouts/v7/modules/Vtiger/ModalHeader.tpl',
+      1 => 1667830458,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '16953488096369195e4e1515-21273484',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'TITLE' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_6369195e4e2e2',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_6369195e4e2e2')) {function content_6369195e4e2e2($_smarty_tpl) {?>
+<div class="modal-header"><div class="clearfix"><div class="pull-right " ><button type="button" class="close" aria-label="Close" data-dismiss="modal"><span aria-hidden="true" class='fa fa-close'></span></button></div><h4 class="pull-left"><?php echo $_smarty_tpl->tpl_vars['TITLE']->value;?>
+</h4></div></div>    <?php }} ?>
